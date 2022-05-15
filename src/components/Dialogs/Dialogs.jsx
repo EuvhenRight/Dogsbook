9ff91f649d02239a0cba2d529e794343d73ce305
EuvhenRight <ugnivenko.ea@gmail.com>
2022-05-15 16:56:01 +0200

    Lesson_36

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -10,13 +10,13 @@ const Dialogs = (props) => {
         props.state.dialogs.map(d => <DialogsItems id={d.id} name={d.name} avatar={d.avatar} />);
 
 
-    let messagesElements = props.state.messages.map(m => <MessagesItems
-        id={m.id} message={m.message}
-    // messages={props.state.messages}
-    // newMessageText={props.state.newMessageText}
-    // updateNewPostMessage={props.updateNewPostMessage}
-    // DialogMessage={props.DialogMessage}
-    />);
+    let messagesElements =
+        props.state.messages.map(m => <MessagesItems id={m.id} message={m.message}
+        // messages={props.state.messages}
+        // newMessageText={props.state.newMessageText}
+        // updateNewPostMessage={props.updateNewPostMessage}
+        // DialogMessage={props.DialogMessage}
+        />);
 
     return (
         <div className={classes.dialogs}>
